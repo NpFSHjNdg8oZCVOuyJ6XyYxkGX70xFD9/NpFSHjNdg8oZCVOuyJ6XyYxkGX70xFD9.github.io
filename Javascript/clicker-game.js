925a3bfed1f6f6dc1f7161f4b7bed3e4425272ff
Reyaank62 <reyaan.kalsheker29@student.tamdistrict.org>
2025-11-20 19:35:20 +0000

    Added upgrade 3

--- Javascript/clicker-game.js
+++ Javascript/clicker-game.js
@@ -2,16 +2,20 @@ let score = 0;
 let autoclickers = 0;
 let clickPower = 1;
 let doubleClickers = 0;
+let employeeDaniel = 0;
 
 let autoClickerCost = 50;
 let doubleClickerCost = 200;
+let employeeDanielCost = 1000;
 
 let scoreDisplay = document.getElementById('score');
 let clickButton = document.getElementById('clickButton');
 let upgrade1Button = document.getElementById('upgrade1');
 let upgrade2Button = document.getElementById('upgrade2');
+let upgrade3Button = document.getElementById('upgrade3');
 let autoClickerDisplay = document.getElementById('autoClickers');
 let doubleClickerDisplay = document.getElementById('doubleClicks');
+let employeeDanielDisplay = document.getElementById('employeeDaniels');
 
 clickButton.onclick = function () {
     score = score + clickPower;
@@ -40,6 +44,20 @@ upgrade2Button.onclick = function () {
         upgrade2Button.textContent = 'Double Click Power (Cost: ' + doubleClickerCost + ')';
     }
 };
+
+upgrade3Button.onclick = function () {
+    if (score >= employeeDanielCost) {
+        score = score - employeeDanielCost;
+        employeeDaniel = employeeDaniel + 1;
+        autoclickers = autoclickers + 5;
+        employeeDanielCost = Math.floor(employeeDanielCost * 1.5);
+        scoreDisplay.textContent = score;
+        employeeDanielDisplay.textContent = employeeDaniel;
+        autoClickerDisplay.textContent = autoclickers;
+        upgrade3Button.textContent = "Hire Employee Daniel (Cost: " + employeeDanielCost + ")";
+    }
+};
+
 
 setInterval(function () {
     score = score + autoclickers;
