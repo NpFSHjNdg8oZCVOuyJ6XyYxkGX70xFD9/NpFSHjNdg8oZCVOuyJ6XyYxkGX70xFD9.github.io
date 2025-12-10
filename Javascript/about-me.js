b92e636c0d3f917cec95da6a0ac6b8a78f154151
Reyaank62 <reyaan.kalsheker29@student.tamdistrict.org>
2025-12-10 03:29:16 +0000

    Added links to sports

---
+++ Javascript/about-me.js
@@ -0,0 +1,11 @@
+document.getElementById("fencing").addEventListener("click", function() {
+    window.open("https://en.wikipedia.org/wiki/Fencing");
+});
+
+document.getElementById("archery").addEventListener("click", function() {
+    window.open("https://en.wikipedia.org/wiki/Archery");
+});
+
+document.getElementById("badminton").addEventListener("click", function() {
+    window.open("https://en.wikipedia.org/wiki/Badminton");
+});
