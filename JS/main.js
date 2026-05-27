85f4cf877247625710ec9edbe90525e4d76f1ce6
Reyaan Kalsheker <reyaan.kalsheker29@student.tamdistrict.org>
2026-05-27 19:23:18 +0000

    Removed alerts

--- JS/main.js
+++ JS/main.js
@@ -34,14 +34,6 @@ document.addEventListener('DOMContentLoaded', function() {
       const cleanedText = priceText.replace('$', '').replace(',', '');
       const price = Number(cleanedText);
 
-      if (money >= price) {
-        money = money - price;
-        moneyDisplay.innerText = 'Money: $' + money;
-        alert('Item purchased!');
-      } else {
-        alert('Not enough money!');
-      }
-
     });
   });
 
