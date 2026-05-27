74d42801d5d8a0bd26e3db0ffa92f28481d6d1c3
Reyaan Kalsheker <reyaan.kalsheker29@student.tamdistrict.org>
2026-05-27 19:18:50 +0000

    Fixed issues with selector

--- JS/main.js
+++ JS/main.js
@@ -1,24 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var tabs = document.querySelectorAll('.tab');
-    var categories = document.querySelectorAll('.category');
+document.addEventListener('DOMContentLoaded', () => {
+    const tabs = document.querySelectorAll('.tab');
+    const categories = document.querySelectorAll('.category');
 
-    tabs.forEach(function(tab) {
-        tab.addEventListener('click', function() {
-            var categoryName = tab.getAttribute('data-category');
-
-            categories.forEach(function(category) {
-                category.style.display = 'none';
-            });
-
-            tabs.forEach(function(t) {
-                t.classList.remove('active');
-            });
-
-            document.getElementById(categoryName).style.display = 'flex';
+    tabs.forEach(tab => {
+        tab.addEventListener('click', () => {
+            categories.forEach(category => category.style.display = 'none');
+            tabs.forEach(t => t.classList.remove('active'));
+            
+            document.getElementById(tab.dataset.category).style.display = 'flex';
             tab.classList.add('active');
         });
     });
 
-
-    document.querySelector('.tab').click();
+    if (tabs.length > 0) {
+        tabs[0].click();
+    }
 });
